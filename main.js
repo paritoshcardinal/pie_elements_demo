ffb4d33fb0d381152db75171114cfdf7597f475f
edeustace <ed.eustace@gmail.com>
2018-07-11 12:52:19 -0400

    fix conflict

--- main.js
+++ main.js
@@ -15,20 +15,16 @@ function renderPIE(model, elementName) {
   }, 500);
 }
 
-<<<<<<< HEAD
 function updateElement(model, id, updateConfig, elementName) {
   const session = getSession(id);
   window["pie-controller-pie-item"][elementName]
-    .model(Object.assign({}, model), session, { mode: "gather" })
+    .model(model, session, { mode: "gather" })
     .then(uiModel => {
       let a = elementName + "[pie-id='" + id + "']";
       let el = window.document.querySelector(a);
       if (el) {
-        //NOTE: moved session above model - this is a bug in the categorize element. The order in which you set the properties shouldn't matter - please raise a bug if this is still an issue with the latest categorize element.
         el.session = session;
         el.model = uiModel;
-      } else {
-        throw new Error("cant find element");
       }
       //below code show be run when you want to create config object
       if (updateConfig) {
@@ -36,7 +32,7 @@ function updateElement(model, id, updateConfig, elementName) {
         let configEl = window.document.querySelector(b);
         if (configEl) {
           //set the raw model on config - it want's all the info.
-          configEl.model = model;
+          configEl.model = uiModel;
           configEl.addEventListener("model.updated", e => {
             updateElement(e.detail.update, id, false, elementName);
           });
@@ -44,29 +40,3 @@ function updateElement(model, id, updateConfig, elementName) {
       }
     });
 }
-=======
-    function updateElement(model, id,updateConfig,elementName) {
-        const session = getSession(id);
-        window['pie-controller-pie-item'][elementName].model(model, session, { mode: 'gather' }).then(uiModel => {
-			
-            let a = elementName+"[pie-id='" + id + "']";
-            let el = window.document.querySelector(a);
-            if (el) {
-				el.session = session;
-                el.model = uiModel;
-            }
-            //below code show be run when you want to create config object
-            if (updateConfig) {
-                let b = elementName+"-configure[pie-id='" + id + "']";
-                let configEl = window.document.querySelector(b);
-                if (configEl) {
-                    //set the raw model on config - it want's all the info.
-                    configEl.model = uiModel;
-                    configEl.addEventListener('model.updated', e => {
-                        updateElement(e.detail.update, id, false,elementName);
-                    });
-                }
-            }
-        });
-    }
->>>>>>> ac2f3848660d68630f1c1f0ea0846d3df56f8a05
