512401e6a484ec6194ca9ce1d7f4881375b04f22
paritosh.cardinal <paritosh.srivastava@cardinalts.com>
2018-07-26 20:48:48 +0530

    Image issue related update

--- main.js
+++ main.js
@@ -37,8 +37,10 @@ function updateElement(model, id, updateConfig, elementName) {
           configEl.addEventListener("model.updated", e => {
             updateElement(e.detail.update, id, false, elementName);
           });
+		  
+
 		  configEl.addEventListener("insert.image", e => {
-            //log('insert.image event received..');
+            this.handleFileSelect = this.handleFileSelect.bind(this);
 			this._insertImageHandler = e.detail;
 			this._$fileInput = window.document.querySelector('input[type="file"]');
 			this._$fileInput.click();
@@ -47,5 +49,47 @@ function updateElement(model, id, updateConfig, elementName) {
       }
     });
 }
+
+function handleFileSelect(e) {
+	let b ="multiple-choice-configure[pie-id='" + id + "']";
+    let configEl = window.document.querySelector(b);
+    const file = event.target.files[0];
+    configEl._insertImageHandler.fileChosen(file);
+    configEl._$fileInput.value = '';
+    var reader = new FileReader();
+    reader.onload = () => {
+      const dataURL = reader.result;
+      /** simulate a delay in uploading */
+      setTimeout(() => {
+        configEl._insertImageHandler.done(null, dataURL);
+        configEl._insertImageHandler = null;
+      }, 2000);
+    };
+
+    let progress = 0;
+    configEl._insertImageHandler.progress(progress);
+
+    range(1, 100).forEach(n => {
+      setTimeout(() => {
+        configEl._insertImageHandler.progress(n);
+      }, n * 20);
+    });
+
+    log('[handleFileSelect] reader.readAsDataUrl, file:', file);
+    reader.readAsDataURL(file);
+  }
+  
+  function connectedCallback() {
+	let b ="multiple-choice-configure[pie-id='" + id + "']";
+    let configEl = window.document.querySelector(b);
+    configEl._$fileInput.addEventListener('change', this.handleFileSelect);
+  }
+
+  function disconnectedCallback() {
+	let b ="multiple-choice-configure[pie-id='" + id + "']";
+    let configEl = window.document.querySelector(b);
+    configEl._$fileInput.removeEventListener('change', this.handleFileSelect);
+  }
+
 
 
